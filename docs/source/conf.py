e5862626f10b71d0dd37e7ccfa5a23df8351208f
Khan <augustinolara311@gmail.com>
2025-04-03 01:47:38 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'XML Tutorials'
+copyright = '2025, xml'
+author = 'Agustino Lrra sheeeshh'
 
 release = '0.1'
 version = '0.1.0'
